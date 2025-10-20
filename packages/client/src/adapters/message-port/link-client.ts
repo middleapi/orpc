4983e45ff40628f7cef30b02306bd1ac3cc7fada
unnoq <contact@unnoq.com>
2025-10-20 16:11:18 +0700

    wip

--- packages/client/src/adapters/message-port/link-client.ts
+++ packages/client/src/adapters/message-port/link-client.ts
@@ -1,24 +1,55 @@
+import type { Promisable, Value } from '@orpc/shared'
 import type { StandardLazyResponse, StandardRequest } from '@orpc/standard-server'
+import type { DecodedRequestMessage, DecodedResponseMessage } from '@orpc/standard-server-peer'
 import type { ClientContext, ClientOptions } from '../../types'
 import type { StandardLinkClient } from '../standard'
 import type { SupportedMessagePort } from './message-port'
-import { ClientPeer } from '@orpc/standard-server-peer'
+import { value } from '@orpc/shared'
+import { experimental_ClientPeerWithoutCodec as ClientPeerWithoutCodec, decodeResponseMessage, encodeRequestMessage } from '@orpc/standard-server-peer'
 import { onMessagePortClose, onMessagePortMessage, postMessagePortMessage } from './message-port'
 
 export interface LinkMessagePortClientOptions {
   port: SupportedMessagePort
+
+  /**
+   * Specify transferable objects (like ArrayBuffers, MessagePorts)
+   * that should be transferred rather than cloned when sending messages.
+   *
+   * @remarks
+   * - return null | undefined to disable this feature
+   *
+   * @warning Make sure your message port supports `transfer` before using this feature.
+   * @example
+   * ```ts
+   * experimental_transfer: (message) => {
+   *   return deepFindTransferableObjects(message)
+   * }
+   * ```
+   */
+  experimental_transfer?: Value<Promisable<object[] | null | undefined>, [message: DecodedRequestMessage]>
 }
 
 export class LinkMessagePortClient<T extends ClientContext> implements StandardLinkClient<T> {
-  private readonly peer: ClientPeer
+  private readonly peer: ClientPeerWithoutCodec
 
   constructor(options: LinkMessagePortClientOptions) {
-    this.peer = new ClientPeer((message) => {
-      return postMessagePortMessage(options.port, message)
+    this.peer = new ClientPeerWithoutCodec(async (message) => {
+      const transfer = options.experimental_transfer && await value(options.experimental_transfer, message)
+
+      if (transfer) {
+        return postMessagePortMessage(options.port, message, transfer)
+      }
+
+      const [id, type, payload] = message
+      return postMessagePortMessage(options.port, await encodeRequestMessage(id, type, payload))
     })
 
     onMessagePortMessage(options.port, async (message) => {
-      await this.peer.message(message)
+      if (Array.isArray(message)) {
+        return await this.peer.message(message as DecodedResponseMessage)
+      }
+
+      return await this.peer.message(await decodeResponseMessage(message))
     })
 
     onMessagePortClose(options.port, () => {
--- packages/server/src/adapters/message-port/handler.ts
+++ packages/server/src/adapters/message-port/handler.ts
@@ -1,32 +1,67 @@
 import type { SupportedMessagePort } from '@orpc/client/message-port'
-import type { MaybeOptionalOptions } from '@orpc/shared'
+import type { MaybeOptionalOptions, Promisable, Value } from '@orpc/shared'
+import type { DecodedRequestMessage, DecodedResponseMessage } from '@orpc/standard-server-peer'
 import type { Context } from '../../context'
 import type { StandardHandler } from '../standard'
 import type {
   HandleStandardServerPeerMessageOptions,
 } from '../standard-peer'
 import { onMessagePortClose, onMessagePortMessage, postMessagePortMessage } from '@orpc/client/message-port'
-import { resolveMaybeOptionalOptions } from '@orpc/shared'
-import { ServerPeer } from '@orpc/standard-server-peer'
+import { resolveMaybeOptionalOptions, value } from '@orpc/shared'
+import { decodeRequestMessage, encodeResponseMessage, experimental_ServerPeerWithoutCodec as ServerPeerWithoutCodec } from '@orpc/standard-server-peer'
 import { createServerPeerHandleRequestFn } from '../standard-peer'
 
+export interface MessagePortHandlerOptions<_T extends Context> {
+  /**
+   * Specify transferable objects (like ArrayBuffers, MessagePorts)
+   * that should be transferred rather than cloned when sending messages.
+   *
+   * @remarks
+   * - return null | undefined to disable this feature
+   *
+   * @warning Make sure your message port supports `transfer` before using this feature.
+   * @example
+   * ```ts
+   * experimental_transfer: (message) => {
+   *   return deepFindTransferableObjects(message)
+   * }
+   * ```
+   */
+  experimental_transfer?: Value<Promisable<object[] | null | undefined>, [message: DecodedResponseMessage]>
+}
+
 export class MessagePortHandler<T extends Context> {
+  private readonly transfer: MessagePortHandlerOptions<T>['experimental_transfer']
+
   constructor(
     private readonly standardHandler: StandardHandler<T>,
+    options: NoInfer<MessagePortHandlerOptions<T>> = {},
   ) {
+    this.transfer = options.experimental_transfer
   }
 
   upgrade(
     port: SupportedMessagePort,
     ...rest: MaybeOptionalOptions<HandleStandardServerPeerMessageOptions<T>>
   ): void {
-    const peer = new ServerPeer((message) => {
-      return postMessagePortMessage(port, message)
+    const peer = new ServerPeerWithoutCodec(async (message) => {
+      const transfer = this.transfer && await value(this.transfer, message)
+
+      if (transfer) {
+        return postMessagePortMessage(port, message, transfer)
+      }
+
+      const [id, type, payload] = message
+      return postMessagePortMessage(port, await encodeResponseMessage(id, type, payload))
     })
 
     onMessagePortMessage(port, async (message) => {
+      if (Array.isArray(message)) {
+        return await peer.message(message as DecodedRequestMessage)
+      }
+
       await peer.message(
-        message,
+        await decodeRequestMessage(message),
         createServerPeerHandleRequestFn(this.standardHandler, resolveMaybeOptionalOptions(rest)),
       )
     })
