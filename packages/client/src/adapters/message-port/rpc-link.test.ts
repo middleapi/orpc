8a0515cc7dd751bb4523fb807f318f427e73dd50
Định Lê <dinwwwh@gmail.com>
2026-07-13 14:40:25 +0700

    feat: upgrade standardserver to 0.4.2 (#1667)

--- packages/client/src/adapters/message-port/rpc-link.test.ts
+++ packages/client/src/adapters/message-port/rpc-link.test.ts
@@ -64,7 +64,6 @@ describe('rpcLink', () => {
     expect(decoded.message.json).toEqual({
       url: '/ping',
       body: { json: 'input' },
-      bodyHint: 'json',
       headers: {},
       method: 'POST',
     })
