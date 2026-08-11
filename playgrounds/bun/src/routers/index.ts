23888e95aaa9bbc3c6a4bcc2adefbaeca812899e
aldotestino <aldotestino4@gmail.com>
2026-08-11 12:44:25 +0200

    feat(bun): add QUERY search example

--- playgrounds/bun/src/routers/index.ts
+++ playgrounds/bun/src/routers/index.ts
@@ -1,6 +1,6 @@
 import { deleteFile, findFile, uploadFile } from './file'
 import { publishMessage, subscribeMessages } from './message'
-import { createPlanet, deletePlanet, findPlanet, listPlanets, updatePlanet } from './planet'
+import { createPlanet, deletePlanet, findPlanet, listPlanets, searchPlanets, updatePlanet } from './planet'
 
 export const router = {
   file: {
@@ -11,6 +11,7 @@ export const router = {
 
   planet: {
     list: listPlanets,
+    search: searchPlanets,
     find: findPlanet,
     create: createPlanet,
     update: updatePlanet,
